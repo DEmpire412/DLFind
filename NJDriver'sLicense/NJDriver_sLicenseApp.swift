0195c45a050e9905e8f5b574acddf670e4a6ad12
DEmpire412 <davidempire412@gmail.com>
2022-11-17 14:48:44 -0500

    Initial Commit

---
+++ NJDriver'sLicense/NJDriver_sLicenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  NJDriver_sLicenseApp.swift
+//  NJDriver'sLicense
+//
+//  Created by David Empire on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct NJDriver_sLicenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
